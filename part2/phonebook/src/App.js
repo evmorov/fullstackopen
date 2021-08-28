cde2f1410e1e23a5cf322f79bb1767398122c810
Evgenii Morozov <evmorov@gmail.com>
2021-08-28 22:42:02 +0200

    2.19

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,18 +3,25 @@ import Filter from './Filter';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
 import personService from './services/persons';
+import Notification from './Notification';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filterWord, setFilterWord] = useState('');
+  const [notificationMessage, setNotificationMessage] = useState(null);
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
       setPersons(initialPersons);
     });
   }, []);
+
+  const showNotification = (message) => {
+    setNotificationMessage(message);
+    setTimeout(() => setNotificationMessage(null), 5000);
+  };
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -31,10 +38,11 @@ const App = () => {
       return;
     }
 
-    personService.create(personObject).then((returnedPerson) => {
+    return personService.create(personObject).then((returnedPerson) => {
       setPersons(persons.concat(returnedPerson));
       setNewName('');
       setNewNumber('');
+      showNotification(`Added ${returnedPerson.name}`);
     });
   };
 
@@ -42,11 +50,13 @@ const App = () => {
     const confirmReplaceMessage = `${persistedPerson.name} is already added to phonebook, replace the old number with a new one?`;
     if (!window.confirm(confirmReplaceMessage)) return;
 
-    updatePerson(persistedPerson.id, personObject);
+    updatePerson(persistedPerson.id, personObject).then(() => {
+      showNotification(`Updated ${persistedPerson.name}`);
+    });
   };
 
   const updatePerson = (id, personObject) => {
-    personService.update(id, personObject).then((returnedPerson) => {
+    return personService.update(id, personObject).then((returnedPerson) => {
       setPersons(persons.map((p) => (p.id === id ? returnedPerson : p)));
       setNewName('');
       setNewNumber('');
@@ -57,8 +67,9 @@ const App = () => {
     const toDelete = persons.find((person) => person.id === id);
     if (!window.confirm(`Delete ${toDelete.name}?`)) return;
 
-    personService.destroy(id).then(() => {
+    return personService.destroy(id).then(() => {
       setPersons(persons.filter((person) => person !== toDelete));
+      showNotification(`Deleted ${toDelete.name}`);
     });
   };
 
@@ -81,6 +92,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notificationMessage} />
       <Filter filterWord={filterWord} onChange={handleFilterChange} />
 
       <h3>Add a new</h3>
