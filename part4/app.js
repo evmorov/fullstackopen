7dd5a1680481e7d55f1f42451ab96a308315e45e
Evgenii Morozov <evmorov@gmail.com>
2021-09-25 22:32:18 +0200

    4.2

---
+++ part4/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use('/api/blogs', blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
+
+module.exports = app;
