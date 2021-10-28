a2164fec7c0c26b273e1dbe76bae0022db06cea6
Evgenii Morozov <evmorov@gmail.com>
2021-10-29 00:23:32 +0200

    4.10

--- part4/tests/blog.test.js
+++ part4/tests/blog.test.js
@@ -34,6 +34,25 @@ describe('when there is initially some notes saved', () => {
 
     expect(response.body[0].id).toBeDefined();
   });
+
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Title from test',
+      author: 'Author from test',
+    };
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+    const titles = blogsAtEnd.map((b) => b.title);
+    expect(titles).toContain('Title from test');
+  });
 });
 
 afterAll(() => {
