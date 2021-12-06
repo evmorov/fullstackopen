a4b23648460d40a73e9eff59ba93bfb6099f5e46
Evgenii Morozov <evmorov@gmail.com>
2021-12-06 10:00:53 +0100

    Use token base auth

--- part4/api-requests/blogs/create.sh
+++ part4/api-requests/blogs/create.sh
@@ -5,10 +5,12 @@ CUR_DIR="$(dirname "$0")"
 . "$CUR_DIR/../helper.sh"
 
 rand=$RANDOM
+token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlVzZXJuYW1lIDE1ODk5IiwiaWQiOiI2MWFkY2M2ZjExYmQ3M2RlYjc0YmJmZjEiLCJpYXQiOjE2Mzg3ODA4ODV9.moZpNNABfKNwZ4-qF5xzvRmWBsLWLgKaQ_2xc857704
 
 printResponse "$(
   $CURL \
     POST "$URL/api/blogs" \
     --header 'Content-Type: application/json' \
-    --data '{ "title": "Title '$rand'", "author": "Author '$rand'", "url": "https://'$rand'.com", "likes": '$rand', "userId": "61ad3adea57c840e83bd72ff" }'
+    --header "Authorization: bearer $token" \
+    --data '{ "title": "Title '$rand'", "author": "Author '$rand'", "url": "https://'$rand'.com", "likes": '$rand' }'
 )"
--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -8,24 +17,27 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  const body = request.body
-
-  const user = await User.findById(body.userId)
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user ? user._id : null,
-  })
-
   try {
-    const savedBlog = await blog.save()
-    if (user) {
-      user.blogs = user.blogs.concat(savedBlog._id)
-      await user.save()
+    const body = request.body
+
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
     }
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id,
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog)
   } catch (exception) {
     next(exception)
