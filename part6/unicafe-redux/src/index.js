1778e21787b2e4f593618815c46afe9c740aef4b
Evgenii Morozov <evmorov@gmail.com>
2021-12-31 16:48:22 +0100

    6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,5 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { createStore } from 'redux'
+import counterReducer from './reducers/counterReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(counterReducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({ type: 'GOOD' })
+  }
+
+  const ok = () => {
+    store.dispatch({ type: 'OK' })
+  }
+
+  const bad = () => {
+    store.dispatch({ type: 'BAD' })
+  }
+
+  const reset = () => {
+    store.dispatch({ type: 'ZERO' })
+  }
+
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button onClick={ok}>ok</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
