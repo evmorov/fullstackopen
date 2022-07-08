0a292efeeb154cb045cb542dadb5274cd1ec865d
Evgenii Morozov <evmorov@gmail.com>
2022-07-08 14:40:18 +0200

    8.19

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useApolloClient } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from './queries'
 
-const Books = (props) => {
-  if (!props.show) {
+const Books = ({ show }) => {
+  const ALL = 'all'
+
+  if (!show) {
     return null
   }
 
+  const [genre, setGenre] = useState(ALL)
   const result = useQuery(ALL_BOOKS)
 
   if (result.loading) {
-    return <div>loading...</div>
+    return <div>Loading...</div>
   }
 
   const books = result.data.allBooks
+  const genres = [ALL, ...new Set(books.map((book) => book.genres).flat())]
 
   return (
     <div>
       <h2>Books</h2>
+      {genres.map((g) => (
+        <button key={g} onClick={() => setGenre(g)}>
+          {g}
+        </button>
+      ))}
+
+      <div>
+        In genre <strong>{genre}</strong>
+      </div>
 
       <table>
         <tbody>
@@ -25,14 +38,18 @@ const Books = (props) => {
             <th></th>
             <th>Author</th>
             <th>Published</th>
+            <th>Genres</th>
           </tr>
-          {books.map((b) => (
-            <tr key={b.id}>
-              <td>{b.title}</td>
-              <td>{b.author.name}</td>
-              <td>{b.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter((b) => (genre !== ALL ? b.genres.includes(genre) : b))
+            .map((b) => (
+              <tr key={b.id}>
+                <td>{b.title}</td>
+                <td>{b.author.name}</td>
+                <td>{b.published}</td>
+                <td>{b.genres.join(', ')}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
